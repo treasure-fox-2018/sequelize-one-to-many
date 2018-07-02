68da62ce353d844ea641c0ca9dbe424256fc9d5b
Rhesa Utomo <venderblitz@gmail.com>
2018-07-02 12:00:04 +0700

    release 2 finished

--- models/teacher.js
+++ models/teacher.js
@@ -3,7 +3,26 @@ module.exports = (sequelize, DataTypes) => {
   var Teacher = sequelize.define('Teacher', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Email format is not valid!'
+        },
+        isUnique(value, next) {
+          Teacher.findOne({
+            where: { email: value }
+          }).then(data => {
+            if (data) {
+              next('Email already exists')
+            } else {
+              next()
+            }
+          }).catch(err => console.log(err))
+        }
+      }
+    },
     subject_id: DataTypes.INTEGER
   }, {});
   Teacher.associate = function(models) {
