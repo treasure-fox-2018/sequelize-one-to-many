dedf5f154a2bcc8c8cc4859e6cae8fabacdce3bf
Rhesa Utomo <venderblitz@gmail.com>
2018-07-02 14:17:21 +0700

    release 2: added bootstrap

--- models/teacher.js
+++ models/teacher.js
@@ -34,3 +34,16 @@ module.exports = (sequelize, DataTypes) => {
   };
   return Teacher;
 };
+
+//  Student.belongsToMany(models.Subject, {
+//    through: 'StudentSubject'
+//  })
+
+// Subject.belongsToMany(models.Student, {
+//   through: 'StudentSubject'
+// })
+
+// on conjunction table
+// --- Use this if you wanted to query data from conjunction table
+// StudentSubject.belongsTo(Student)
+// StudentSubject.belongsTo(Subject)
